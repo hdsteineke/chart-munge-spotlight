c29258634fb4e8abcec2ed2a850591e4c708d91d
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-04-01 10:22:10 -0700

    chart data populating correctly on live server

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 // import functions and grab DOM elements
-
+import { purchaseFrequency } from './data-utils.js';
 // let state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const purchases = purchaseFrequency();
 
+const labels = Object.keys(purchases);
 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
+const values = Object.values(purchases);
+
+console.log(labels, values);
 
 const data = {
     labels: labels,
@@ -23,17 +20,17 @@ const data = {
         label: 'My First dataset',
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
+        data: values,
     }]
 };
 
 const config = {
-    type: 'line',
+    type: 'bar',
     data: data,
     options: {}
 };
 
-
+// eslint-disable-next-line
 const myChart = new Chart(
     document.getElementById('myChart'),
     config
---
+++ data-utils.js
@@ -0,0 +1,24 @@
+import { customers } from './data.js';
+
+
+
+export function purchaseFrequency() {
+
+    const countMap = {};
+
+    for (let customer of customers) {
+        const frequency = customer.purchase_frequency;
+
+        if (countMap[frequency]) {
+
+            countMap[frequency]++;
+
+        } else {
+
+            countMap[frequency] = 1;
+        }
+    }
+
+    return countMap;
+
+}
